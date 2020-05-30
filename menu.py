924233965510a4450de2e3517e3158c3dd3edb52
mvalleym <martinvallejom@gmail.com>
2020-05-30 12:52:25 -0300

    adaptacion superficial a PEP 8

--- menu.py
+++ menu.py
@@ -6,14 +6,20 @@ def confign_nuevo_juego(configuracion):
     s = []
     for i in range(1,61):
         s.append(str(i))
-    layout = [
 
-        [sg.T("Configuraciones", size=(17,1), justification = "center", font=("Georgia", 17))],
-        [sg.T("Dificultad: "),sg.DropDown(('Facil','Medio','Dificil'),default_value=(configuracion['dificultad']),size=(10,1))],
-        [sg.T("Tiempo de juego (en minutos): ", size=(22, 1)),sg.InputCombo((s),size=(5,1),default_value=(configuracion['tiempo']))],
+    layout = [
+        [sg.T("Configuraciones", size=(17,1), justification = "center",
+              font=("Georgia", 17))],
+        [sg.T("Dificultad: "),
+         sg.DropDown(('Facil','Medio','Dificil'),
+                     default_value=(configuracion['dificultad']),size=(10,1))],
+        [sg.T("Tiempo de juego (en minutos): ", size=(22, 1)),
+         sg.InputCombo((s),size=(5,1),default_value=(configuracion['tiempo']))],
         [sg.B("Jugar", size=(17, 1), key="-jugar-")]
-    ]
+        ]
+
     window = sg.Window("ScrabbleAR - Configuracion", layout)
+
     while True:
         event, value = window.read()
         print(event,value)
@@ -23,14 +29,18 @@ def confign_nuevo_juego(configuracion):
             break
         else:
             break
+
     tableros.jugar()
+
     window.close()
 
     return(configuracion)
 
 def pantalla_inicial():
+
     layout = [
-        [sg.T("Scrabble AR", size=(17, 1), justification="center", font=("Georgia", 17))],
+        [sg.T("Scrabble AR", size=(17, 1), justification="center",
+              font=("Georgia", 17))],
         [sg.T(' '),sg.B('Nueva partida', size=(25, 2), key="-nueva-")],
         [sg.T(' '),sg.B('Continuar partida', size=(25, 2), key="-continuar-")],
         [sg.T(' '),sg.B('Configuración', size=(25, 2), key="-configuracion-")],
@@ -38,6 +48,7 @@ def pantalla_inicial():
         [sg.T(' '),sg.B('Salir', size=(25, 2), key="-salir-")],
         [sg.T(' '),sg.T("  ", size=(17, 1), justification="center",)]
         ]
+
     window = sg.Window("ScrabbleAR - Menu", layout)
     
     while True:
@@ -52,11 +63,11 @@ def pantalla_inicial():
                 json.dump(configs, f, indent= 4)
             break
         elif event in "-continuar-":
-            None
+            pass
         elif event in "-configuracion-":
-            None
+            pass
         elif event in "-puntajes-":
-            None
+            pass
         
     window.close()
 
