43459794288a08e3a1e1cac60a86d0610bccdd47
mvalleym <mvalleym@users.noreply.github.com>
2020-09-10 13:34:11 -0300

    Update partida.py

--- layouts/partida.py
+++ layouts/partida.py
@@ -29,19 +29,19 @@ def armar_atril(atril, dim_boton):
 
 
 def columna1_gui(elementos):
-    dim_boton = 28, 28
+    dim_boton = 30, 30
     linea_superior = armar_atril(elementos['atril_ia'], dim_boton)
     botones_tablero = armar_tablero(elementos['tablero'], dim_boton)
     linea_inferior = armar_atril(elementos['atril_jugador'], dim_boton)
-    botones_jugador = [sg.T(' '),
+    botones_jugador = [sg.T(f'{"".join([" "]*12)}', font=('Arial', '11')),
                        sg.Button('JUGAR', key='-JUGAR-', size=(9, 2),
-                                 disabled=True),
+                                 disabled=True, font=('Arial', '9')),
                        sg.Button('PASAR\nTURNO', key='-PASAR-', size=(9, 2),
-                                 disabled=True),
+                                 disabled=True, font=('Arial', '9')),
                        sg.Button('CAMBIAR\nFICHAS', key='-CAMBIAR-', size=(9, 2),
-                                 disabled=True),
+                                 disabled=True, font=('Arial', '9')),
                        sg.Button('CANCELAR', key='-CANCELAR-', size=(9, 2),
-                                 visible=False)
+                                 visible=False, font=('Arial', '9'))
                        ]
     for item in botones_jugador:
         linea_inferior.append(item)
@@ -51,8 +51,9 @@ def columna1_gui(elementos):
                 [sg.T('')],
                 *botones_tablero,
                 [sg.T('')],
-                [sg.T('FICHAS JUGADOR:'), sg.T(f'{"".join([" "]*83)}'),
-                 sg.T('CAMBIOS SIN COSTO:'), sg.T('5', key='-CAMBIOS-')],
+                [sg.T('FICHAS JUGADOR:'), sg.T(f'{"".join([" "]*51)}'),
+                 sg.T('CAMBIOS SIN COSTO:', font=('Arial', '9')),
+                 sg.T('5', key='-CAMBIOS-', font=('Arial', '9'))],
                 linea_inferior]
     return columna1
 
@@ -61,30 +62,30 @@ def columna2_gui(datos):
     columna2 = [[sg.Button('COMENZAR', key='-INI/PAUSA-', size=(12, 2)),
                  sg.Button('TERMINAR', key='-FIN-', size=(12, 2),
                            visible=False)],
-                [sg.T('')],
-                [sg.T('TIEMPO', font=('Arial', '11'))],
+                [sg.T('', font=('Arial', '6'))],
+                [sg.T('TIEMPO', font=('Arial', '10'))],
                 [sg.T(datos['reloj'], key='-RELOJ-',
-                      font=('Arial', '18'))],
-                [sg.T('')],
-                [sg.T('DIFICULTAD:', font=('Arial', '11'))],
+                      font=('Arial', '16'))],
+                [sg.T('', font=('Arial', '6'))],
+                [sg.T('DIFICULTAD:', font=('Arial', '10'))],
                 [sg.T(datos['dificultad'], key='-dificultad-',
-                      font=('Arial', '18'))],
-                [sg.T('')],
-                [sg.T('TURNO', font=('Arial', '11'))],
+                      font=('Arial', '16'))],
+                [sg.T('', font=('Arial', '6'))],
+                [sg.T('TURNO', font=('Arial', '10'))],
                 [sg.T('-', size=(10, 1), key='-TURNO-',
                       font=('Arial', '14'))],
-                [sg.T('')],
-                [sg.T('PUNTAJES', font=('Arial', '11'))],
+                [sg.T('', font=('Arial', '6'))],
+                [sg.T('PUNTAJES', font=('Arial', '10'))],
                 [sg.T(f'{datos["nombre"].upper()}:', size=(15, 1),
                       key='-PJUGADOR-', font=('Arial', '14'))],
                 [sg.T('IA:', size=(15, 1), key='-PIA-',
                       font=('Arial', '14'))],
-                [sg.T('')],
-                [sg.T('BOLSA', font=('Arial', '11'))],
+                [sg.T('', font=('Arial', '6'))],
+                [sg.T('BOLSA', font=('Arial', '10'))],
                 [sg.T(f'QUEDAN {len(datos["bolsa"].fichas)} FICHAS',
                       key='-BOLSA-', font=('Arial', '14'))],
-                [sg.T('')],
-                [sg.T('HISTORIAL')],
-                [sg.Output(size=(15, 15))]
+                [sg.T('', font=('Arial', '6'))],
+                [sg.T('HISTORIAL', font=('Arial', '10'))],
+                [sg.Output(size=(22, 10))]
                 ]
     return columna2
