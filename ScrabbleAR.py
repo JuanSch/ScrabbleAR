1cc4bad75b5dd458529474643d51f3f67bedfc5c
Juan Schachner <57379736+JuanSch@users.noreply.github.com>
2020-10-06 23:29:43 -0300

    update

--- ScrabbleAR.py
+++ ScrabbleAR.py
@@ -156,7 +156,7 @@ def configurar():
             window_configurar.find_element(1).Update(valor_letra)
             valor_punto = dic['Personalizada']['puntos_letra'][values[0]]
             window_configurar.find_element(2).Update(valor_punto)
-        except KeyError:
+        except:
             break
         if event == None:
             break
